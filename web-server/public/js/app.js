4423dd1d2914ebf9c1c2586a54f40e835c76455f
gopalsharma05 <rohitsharma050498@gmail.com>
2020-09-01 21:50:21 +0530

    Update app.js

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,13 +7,14 @@ console.log("client side javascript code is loaded!")
 
 const weatherform=document.querySelector('form')
 const search=document.querySelector('input')
+const ms1=document.getElementById('m-1');
+const ms2=document.getElementById('m-2');
 
 weatherform.addEventListener('submit',(e)=>{
 	e.preventDefault()  
 	const city=search.value;
 	// console.log(city)
-			const ms1=document.getElementById('m-1');
-			const ms2=document.getElementById('m-2');
+		
 
 	fetch("http://localhost:3000/weather?address="+city).then((response)=>{
 	response.json().then((data)=>{
@@ -23,10 +24,9 @@ weatherform.addEventListener('submit',(e)=>{
 		}
 		else
 		{
-			ms1.innerHTML=data.max_temp;
-			ms2.innerHTML=data.weather_rep;
-		  
-			 
+			ms1.innerHTML="maxm temperature for tommorow will be "+data.max_temp +" and  minimum temperature will be"+data.max_temp;
+			ms2.innerHTML =(JSON.stringify(data.weather));
+			 		 
 
 		}
 
